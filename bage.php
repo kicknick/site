e059a40a74eeff72c23f7808230dc8b6696f15bb
Alexandr Penko <odael.odes@gmail.com>
2015-05-23 20:15:01 +0300

    bage

--- bage.php
+++ bage.php
@@ -1,46 +1,67 @@
 <?php
-	// function win2uni($s)
-	// {
-	// 	$s = convert_cyr_string($s,'w','i'); 
-	// 	for ($result='', $i=0; $i<strlen($s); $i++) {
-	// 		$charcode = ord($s[$i]);
-	// 		$result .= ($charcode>175)?"&#".(1040+($charcode-176)).";":$s[$i];
-	// 	}
-	// 	return $result;
-	// }
+	
+	function LoadJPEGfromPOST($imgname)
+	{
+	    $uploaddir = '.\\tmpimg\\';
+		$uploadfile = $uploaddir . basename($_FILES[$imgname]['name']);
+		if (!move_uploaded_file($_FILES[$imgname]['tmp_name'], $uploadfile)){
+    		/* Создаем пустое изображение */
+	        $im  = imagecreatetruecolor(150, 30);
+	        $bgc = imagecolorallocate($im, 255, 255, 255);
+	        $tc  = imagecolorallocate($im, 0, 0, 0);
 
+	        imagefilledrectangle($im, 0, 0, 150, 30, $bgc);
+
+	        /* Выводим сообщение об ошибке */
+	        imagestring($im, 1, 5, 5, 'Ошибка загрузки ' . $uploadfile, $tc);
+		}
+	    $im = @imagecreatefromjpeg($uploadfile);
+
+	    /* Если не удалось */
+	    if(!$im)
+	    {
+	        /* Создаем пустое изображение */
+	        $im  = imagecreatetruecolor(150, 30);
+	        $bgc = imagecolorallocate($im, 255, 255, 255);
+	        $tc  = imagecolorallocate($im, 0, 0, 0);
+
+	        imagefilledrectangle($im, 0, 0, 150, 30, $bgc);
+
+	        /* Выводим сообщение об ошибке */
+	        imagestring($im, 1, 5, 5, 'Ошибка загрузки ' . $uploadfile, $tc);
+	    }
+
+	    return $im;
+	}
+
+	
+	
+	
 
 	define('FONT_NAME', 'arial.ttf');
 	define('FONT_SIZE', 20);
 
+	@$foto_x = $_GET['foto_x'];
+	@$foto_y = $_GET['foto_y'];
+	@$foto_width = $_GET['foto_width'];
+	@$foto_height = $_GET['foto_height'];
 
-	//@$firstname = $_POST['firstname'];
-	//@$lastname = $_POST['lastname'];
-	//@$middlename = $_POST['middlename'];
+	$foto_x = 0;
+	$foto_y = 0;
+	$foto_width = 300;
+	$foto_height = 400;
 
 	@$firstname = $_GET['firstname'];
 	@$lastname = $_GET['lastname'];
 	@$middlename = $_GET['middlename'];
-
-	//$firstname = "Sasha";
-	//$lastname = "Penko";
-	//$middlename = "Valer";
-
 	$fio = $lastname.' '.$firstname.' '.$middlename;
-	//$fio = win2uni($fio);
 
 
-	$image = imagecreatetruecolor(800,600) // создаем изображение...
+	$foto = LoadJPEGfromPOST('user_pic');
+	$image = imagecreatetruecolor(1000,1000) // создаем изображение...
 	or die('Cannot create image');     // ...или прерываем работу скрипта в случае ошибки
-
+	
 	imagefill($image, 0, 0, 0xFFFFFF);
-
-	//$coord = imagettfbbox(
-    //	20,  // размер шрифта
-	//	0,          // угол наклона шрифта (0 = не наклоняем)
-	//	FONT_NAME,  // имя шрифта, а если точнее, ttf-файла
-	//	$fio       // собственно, текст
-    //);
 
 	imagettftext(
 	    $image,      // как всегда, идентификатор ресурса
@@ -52,20 +73,10 @@
 	    FONT_NAME,   // имя ttf-файла
 	    $fio
   	);
-
-  	// imagettftext(
-	  //   $image,      // как всегда, идентификатор ресурса
-	  //   FONT_SIZE,   	// размер шрифта
-	  //   0,           // угол наклона шрифта
-	  //   0,100,      // координаты (x,y), соответствующие левому нижнему
-	  //                // углу первого символа
-	  //   0x000000,    // цвет шрифта
-	  //   FONT_NAME,   // имя ttf-файла
-	  //   "Лолец"
-  	// );
-
 	header('Content-type: image/png'); 
-	// ...И, наконец, выведем сгенерированную картинку в формате PNG:
+
+	imagecopyresized ($image, $foto, 0, 0, $foto_x, $foto_y, 150, 200, $foto_width, $foto_height);
+
 	imagepng($image);
 
 	imagedestroy($image);                // освобождаем память, выделенную для изображения
--- makebage.php
+++ makebage.php
@@ -22,6 +22,19 @@
 	<button id="button" class="btn btn-default" type="button">Получить бэйдж</button>
 </form>
 </div>
+<div id="content">
+<h1>Отправляем изображение на сервер</h1>
+<form action="bage.php" method="post" enctype="multipart/form-data">
+<fieldset>
+<input type="hidden" name="MAX_FILE_SIZE" value="2000000">
+<label for="user_pic">Отправка изображения:</label>
+<input type="file" name="user_pic" size="30">
+</fieldset>
+<fieldset>
+<input type="submit" value="Отправить изображение">
+</fieldset>
+</form>
+</div>
 <script type="text/javascript">
 
 var LFM = new Array();
